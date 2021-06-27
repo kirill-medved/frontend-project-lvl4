89b4e8445f06528c66881530dc197bd91006b920
Kirill Medved <medved.kirill@bk.ru>
2021-06-27 06:53:08 +0300

    Fix yup schema

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -8,7 +8,7 @@ export default (props) => {
     username: yup.string().required('Email is required'),
     password: yup
       .string()
-      .min(8, 'Password should be of minimum 8 characters length')
+      .min(4, 'Password should be of minimum 8 characters length')
       .required('Password is required'),
   });
 
