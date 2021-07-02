09e46aa2343babe90e8b620e97d9997da87ac4bd
Kirill Medved <medved.kirill@bk.ru>
2021-07-03 02:37:06 +0300

    Added socket.io

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
+import { io } from 'socket.io-client';
+
 import TokenContext from '../../context.js';
 import { setMessages } from '../../store/messagesSlice.js';
 import { setChannels } from '../../store/channelsSlice.js';
@@ -9,6 +11,21 @@ import Messages from './Messages.js';
 import style from './Main.module.sass';
 
 export default (props) => {
+  const socket = io();
+
+  socket.on('connect', () => {
+    console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+  });
+
+  socket.on('disconnect', () => {
+    console.log(socket.id); // undefined
+  });
+
+  const acknowledge = ({ status }) => {
+    if (status === 'ok') {
+    }
+  };
+
   const auth = useContext(TokenContext);
   const instance = axios.create({
     baseURL: '/api/v1/',
@@ -27,7 +44,12 @@ export default (props) => {
 
   const inputHandler = (e) => {
     setMessage(e.target.value);
-  }
+  };
+
+  const formHandler = (e) => {
+    e.preventDefault();
+    socket.emit('newMessage', message, acknowledge);
+  };
   useEffect(async () => {
     const res = await instance.get(`data`);
 
@@ -43,6 +65,9 @@ export default (props) => {
         <Messages messages={messages} />
         <form>
           <input value={message} onChange={inputHandler} />
+          <button type='submit' onClick={formHandler}>
+            submit
+          </button>
         </form>
       </div>
     </div>
