3ce6ba6b5b661a066ac4a8797970eb8aecb7dc8c
Kirill Medved <medved.kirill@bk.ru>
2021-07-05 02:37:58 +0300

    Fix

--- src/Components/Main/Chat/Messages.js
+++ src/Components/Main/Chat/Messages.js
@@ -14,9 +14,7 @@ const Messages = (props) => {
             <div key={message.id} className={style.wrapper}>
               <div>
                 <h4>{message.username}</h4>
-                <h5>
-                  {message.date.getHours()}:{message.date.getMinutes()}
-                </h5>
+                <h5>{message.date.splite(11, -4)} </h5>
               </div>
               <div>
                 <p>{message.message}</p>
