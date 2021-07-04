4a386e822761569b39c318ba13e1ae7f5d3d90be
Kirill Medved <medved.kirill@bk.ru>
2021-07-05 02:24:56 +0300

    Fix

--- src/Components/Main/Chat/Messages.js
+++ src/Components/Main/Chat/Messages.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Login from '../../Login';
 import style from './Messages.module.sass';
 
 const Messages = (props) => {
@@ -6,12 +7,15 @@ const Messages = (props) => {
     <div>
       {props.messages.length > 0 &&
         props.messages.map((message) => {
+          console.log(message.date);
+          console.log(typeof message.date);
+          console.log(message.date.getMinutes());
           return (
             <div key={message.id} className={style.wrapper}>
               <div>
                 <h4>{message.username}</h4>
                 <h5>
-                  {message.date.getMonth()}:{message.date.getMinutes()}
+                  {message.date.getHours()}:{message.date.getMinutes()}
                 </h5>
               </div>
               <div>
