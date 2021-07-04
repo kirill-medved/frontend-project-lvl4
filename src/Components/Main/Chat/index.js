ac745a96f04858e1b24cc1cd4821b55207617d42
Kirill Medved <medved.kirill@bk.ru>
2021-07-04 19:52:48 +0300

    Added sending message

--- src/Components/Main/Chat/index.js
+++ src/Components/Main/Chat/index.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import { io } from 'socket.io-client';
+
 import Messages from './Messages.js';
 import Form from './Form.js';
 import style from './Chat.module.scss';
+import { useDispatch } from 'react-redux';
+import { sendMessage } from '../../../store/messagesSlice.js';
 
 export default (props) => {
+  const socket = io();
+
+  const dispatch = useDispatch();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    socket.emit('newMessage', message, ({ status }) => {
+      status === 'ok' ? console.log('OK') : console.log('False');
+    });
+    socket.on('newMessage', (message) => {
+      dispatch(sendMessage(message));
+    });
+  };
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [input]);
   return (
     <div className={style.wrapper}>
       <Messages messages={props.messages} />
-      <Form />
+      <Form onSubmit={onSubmit} />
     </div>
   );
 };
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,25 +1,15 @@
 import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
-import { io } from 'socket.io-client';
 
 import TokenContext from '../../context.js';
-import { sendMessage, setMessages } from '../../store/messagesSlice.js';
+import { setMessages } from '../../store/messagesSlice.js';
 import { setChannels } from '../../store/channelsSlice.js';
 import Channels from './Channels/Channels.js';
 import style from './Main.module.scss';
 import Chat from './Chat/index.js';
 
 export default (props) => {
-  const socket = io();
-
-  socket.on('connect', () => {
-    console.log(socket.id); // x8WIv7-mJelg7on_ALbx
-  });
-  socket.on('disconnect', () => {
-    console.log(socket.id); // undefined
-  });
-
   const auth = useContext(TokenContext);
   const instance = axios.create({
     baseURL: '/api/v1/',
@@ -28,22 +18,11 @@ export default (props) => {
     },
   });
 
-  const acknowledge = ({ status }) => {
-    console.log(`status message ${status}`);
-  };
-
-  // socket.emit('newMessage', message, acknowledge);
-  // dispatch(sendMessage());
-
   const dispatch = useDispatch();
   const channels = useSelector((state) => state.channels.channels);
   const messages = useSelector((state) => state.messages.messages);
   console.log(channels);
   console.log(messages);
-
-  // useEffect(() => {
-  //   return () => {};
-  // }, []);
 
   useEffect(async () => {
     const res = await instance.get(`data`);
