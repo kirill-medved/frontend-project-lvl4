060e69b6c556c4f8e2341535c60868fc27fe09bd
Kirill Medved <medved.kirill@bk.ru>
2021-07-05 03:26:22 +0300

    Fix

--- src/Components/Main/Channels/Channel.js
+++ src/Components/Main/Channels/Channel.js
@@ -1,15 +1,26 @@
 import React, { useRef } from 'react';
+import classNames from 'classnames/bind.js';
 
+import style from './Channels.module.scss';
 import { setCurrentChannelId } from '../../../store/channelsSlice.js';
 
 const Channel = (props) => {
+  let cx = classNames.bind(style);
   const channelRef = useRef(null);
 
   const changeChannel = (e) => {
+    console.log(channelRef.current.id);
     setCurrentChannelId(channelRef.current.id);
   };
   return (
-    <div ref={channelRef} onClick={changeChannel}>
+    <div
+      ref={channelRef}
+      onClick={changeChannel}
+      className={cx({
+        wrapper__channel: true,
+        active: channel.id === props.currentChannelId,
+      })}
+    >
       <p>{props.name}</p>
     </div>
   );
--- src/Components/Main/Channels/Channels.js
+++ src/Components/Main/Channels/Channels.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import classNames from 'classnames/bind.js';
 
 import style from './Channels.module.scss';
 import Channel from './Channel.js';
 
 const Channels = (props) => {
-  let cx = classNames.bind(style);
-
   return (
     <div className={style.wrapper}>
       {props.channels.length > 0 &&
         props.channels.map((channel) => {
           return (
-            <Channel
-              key={channel.id}
-              id={channel.id}
-              className={cx({
-                wrapper__channel: true,
-                active: channel.id === props.currentChannelId,
-              })}
-              name={channel.name}
-            />
+            <Channel key={channel.id} id={channel.id} name={channel.name} />
           );
         })}
     </div>
