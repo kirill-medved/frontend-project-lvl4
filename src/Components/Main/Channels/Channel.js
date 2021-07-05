6c8ca451f6499d04bd232c4b979d8776f26595ee
Kirill Medved <medved.kirill@bk.ru>
2021-07-05 03:16:31 +0300

    Fix

---
+++ src/Components/Main/Channels/Channel.js
@@ -0,0 +1,18 @@
+import React, { useRef } from 'react';
+
+import { setCurrentChannelId } from '../../../store/channelsSlice.js';
+
+const Channel = (props) => {
+  const channelRef = useRef(null);
+
+  const changeChannel = (e) => {
+    setCurrentChannelId(channelRef.current.id);
+  };
+  return (
+    <div ref={channelRef} onClick={changeChannel}>
+      <p>{channel.name}</p>
+    </div>
+  );
+};
+
+export default Channel;
--- src/Components/Main/Channels/Channels.js
+++ src/Components/Main/Channels/Channels.js
@@ -2,29 +2,24 @@ import React from 'react';
 import classNames from 'classnames/bind.js';
 
 import style from './Channels.module.scss';
-import { setCurrentChannelId } from '../../../store/channelsSlice.js';
+import Channel from './Channel.js';
 
 const Channels = (props) => {
   let cx = classNames.bind(style);
-  const changeChannel = (e) => {
-    setCurrentChannelId(e.targer.id);
-  };
+
   return (
     <div className={style.wrapper}>
       {props.channels.length > 0 &&
         props.channels.map((channel) => {
           return (
-            <div
+            <Channel
               key={channel.id}
               id={channel.id}
               className={cx({
                 wrapper__channel: true,
                 active: channel.id === props.currentChannelId,
               })}
-              onClick={changeChannel}
-            >
-              <p>{channel.name}</p>
-            </div>
+            />
           );
         })}
     </div>
