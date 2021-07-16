f6f0ee00197d9add4b60197327e92ac8fd5d906f
Kirill Medved <medved.kirill@bk.ru>
2021-07-16 03:40:16 +0300

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Container>
+      <Container className='container h-100 my-4 overflow-hidden rounded shadow'>
         <Switch>
           <PrivateRoute path='/' exact>
             <Main />
