86db59b06c80549e76b4306c66752e68fde3927d
Kirill Medved <medved.kirill@bk.ru>
2021-07-17 06:02:31 +0300

    Fix

--- src/Components/Main/Channels/Channel.jsx
+++ src/Components/Main/Channels/Channel.jsx
@@ -191,7 +191,7 @@ const Channel = ({
     >
       <div>
         <p>{name}</p>
-        <p>{lastMessage.message.length > 0 && lastMessage.message}</p>
+        <p>{lastMessage && lastMessage.message}</p>
         {removable && (
           <>
             <NavDropdown>
