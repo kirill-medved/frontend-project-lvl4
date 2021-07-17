ce573df1950d03fbe541d869bed6cd08f20c8a26
Kirill Medved <medved.kirill@bk.ru>
2021-07-17 07:44:55 +0300

    Fix

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -92,7 +92,7 @@ export default () => {
 
   return (
     <Row className='row h-100 bg-white flex-md-row'>
-      <Col md={3} className='bg-light px-0'>
+      <Col md={3} className='col-4 col-md-2 border-end pt-5 px-0 bg-light'>
         <Channels
           channels={channels}
           currentChannelId={currentChannelId}
@@ -100,7 +100,7 @@ export default () => {
           messages={messages}
         />
       </Col>
-      <Col md={9} className='row h-100 flex-md-column'>
+      <Col className='col p-0 h-100'>
         <Chat
           messages={messages}
           currentChannelId={currentChannelId}
