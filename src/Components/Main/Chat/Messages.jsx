a8378575bd6a558ac4d9cd8283ebce001bdbd713
Kirill Medved <medved.kirill@bk.ru>
2021-07-17 10:46:42 +0300

    Fix

--- src/Components/Main/Chat/Messages.jsx
+++ src/Components/Main/Chat/Messages.jsx
@@ -17,20 +17,21 @@ const Messages = ({ messages, username }) => {
       {messages.length > 0 &&
         messages.map((message) => {
           return (
-            <div
-              key={message.id}
-              className={cx({
-                wrapper: true,
-                myMessage: message.username === username,
-                notMyMessage: !(message.username === username),
-              })}
-            >
-              <div className='d-flex justify-content-around'>
-                <div>{message.username}</div>
-                <div>{message.date.slice(11, -5)} </div>
-              </div>
-              <div>
-                <p className={cx({ message: true })}>{message.message}</p>
+            <div key={message.id} className='w-100'>
+              <div
+                className={cx({
+                  wrapper: true,
+                  myMessage: message.username === username,
+                  notMyMessage: !(message.username === username),
+                })}
+              >
+                <div className='d-flex justify-content-around'>
+                  <div>{message.username}</div>
+                  <div>{message.date.slice(11, -5)} </div>
+                </div>
+                <div>
+                  <p className={cx({ message: true })}>{message.message}</p>
+                </div>
               </div>
             </div>
           );
