ecf5592a82207c99b389c89410a0623380751f84
Kirill Medved <medved.kirill@bk.ru>
2021-07-24 06:42:06 +0300

    Fix

--- src/Components/Main/Chat/Form.jsx
+++ src/Components/Main/Chat/Form.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 const Form = ({ onSubmit }) => {
   const [message, setMessage] = useState('');
 
-  const formHandler = () => {
+  const submitBtRef = useRef(null);
+
+  const formHandler = (e) => {
+    e.preventDefault();
     if (message.length > 0) {
       onSubmit(message.trim());
       setMessage('');
@@ -19,9 +22,10 @@ const Form = ({ onSubmit }) => {
     if (e.code === 'Enter' && e.shiftKey) {
       console.log('IIIIIIIIII');
       setMessage(`${message}\n`);
+      return;
     }
-    if (e.code === 'Enter' && !e.shiftKey) {
-      formHandler();
+    if (e.code === 'Enter') {
+      submitBtRef.current.click();
     }
     setMessage(e.target.value);
 
@@ -44,6 +48,7 @@ const Form = ({ onSubmit }) => {
               type='submit'
               onClick={formHandler}
               className='btn btn-outline-secondary'
+              ref={submitBtRef}
             >
               <img
                 src='https://img.icons8.com/material-outlined/24/000000/sent.png'
