779ad5b7420ac922c8ce90fabd70967ea762cf04
Kirill Medved <medved.kirill@bk.ru>
2021-08-01 06:19:13 +0300

    Export modals

---
+++ src/components/modals/index.js
@@ -0,0 +1,5 @@
+import CreateChannelModal from './create-channel';
+import DeleteChannelModal from './delete-channel';
+import RenameChannelModal from './rename-channel';
+
+export { CreateChannelModal, DeleteChannelModal, RenameChannelModal };
